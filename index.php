4fd02703442763974595f85e2d97c868f894fa4d
RolandVinkel <115623120+RolandVinkel@users.noreply.github.com>
2023-10-16 10:36:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,58 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Home For Sale</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 20px 0;
+            text-align: center;
+        }
+        h1 {
+            font-size: 36px;
+        }
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        .property-details {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        }
+        .property-details img {
+            max-width: 100%;
+            border-radius: 5px;
+        }
+    </style>
+</head>
 <body>
-
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
-
+    <header>
+        <h1>Your Home For Sale</h1>
+    </header>
+    <div class="container">
+        <div class="property-details">
+            <h2>Beautiful Home for Sale</h2>
+            <p>Address: 1234 Elm Street, City, State</p>
+            <p>Price: $500,000</p>
+            <p>Bedrooms: 3</p>
+            <p>Bathrooms: 2</p>
+            <p>Size: 2,000 sq. ft.</p>
+            <img src="your-home-image.jpg" alt="Home Image">
+            <p>Description: This is a spacious and well-maintained home in a great location. It features a beautiful backyard, modern kitchen, and much more. Don't miss this opportunity!</p>
+            <p>Contact us for more information: <a href="tel:123-456-7890">123-456-7890</a></p>
+        </div>
+    </div>
 </body>
 </html>
